2d79bad76122e892b053d729594defd537c63754
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2018-12-15 15:09:20 +0700

    initialze pantau bersama app

---
+++ PantauBersama/PantauBersama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PantauBersama
+//
+//  Created by Hanif Sugiyanto on 15/12/18.
+//  Copyright © 2018 PantauBersama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
