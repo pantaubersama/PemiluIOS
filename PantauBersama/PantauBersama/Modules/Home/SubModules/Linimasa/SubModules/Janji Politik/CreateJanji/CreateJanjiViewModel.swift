fc9948b7c49397b8ced9b92e45c9dffdf8ccfc74
wisnu bhakti <>
2019-01-02 02:27:30 +0700

    [Bhakti] [Pilpres] Create Janji politik

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Linimasa/SubModules/Janji Politik/CreateJanji/CreateJanjiViewModel.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Linimasa/SubModules/Janji Politik/CreateJanji/CreateJanjiViewModel.swift
@@ -8,13 +8,22 @@
 
 import RxSwift
 import RxCocoa
+import Networking
+import Common
 
 protocol ICreateJanjiViewModelInput {
     var backI: AnyObserver<Void> { get }
+    var doneI: AnyObserver<Void> { get }
+    var titleI: AnyObserver<String> { get }
+    var bodyI: AnyObserver<String> { get }
+    var viewWillAppearI: AnyObserver<Void> { get }
 }
 
 protocol ICreateJanjiViewModelOutput {
-    
+    var userDataO: Driver<UserResponse>! { get }
+    var enableO: Driver<Bool>! { get }
+    var errorO: Driver<Error>! { get }
+    var doneO: Driver<Void>! { get }
 }
 
 protocol ICreateJanjiViewModel {
@@ -33,16 +42,90 @@ final class CreateJanjiViewModel: ICreateJanjiViewModel, ICreateJanjiViewModelIn
     
     // Input
     var backI: AnyObserver<Void>
+    var doneI: AnyObserver<Void>
+    var titleI: AnyObserver<String>
+    var bodyI: AnyObserver<String>
+    var viewWillAppearI: AnyObserver<Void>
+    
     // Output
+    var userDataO: Driver<UserResponse>!
+    var enableO: Driver<Bool>!
+    var errorO: Driver<Error>!
+    var doneO: Driver<Void>!
     
     // Subject
     private let backS = PublishSubject<Void>()
+    private let doneS = PublishSubject<Void>()
+    private let titleS = PublishSubject<String>()
+    private let bodyS = PublishSubject<String>()
+    private let viewWillAppearS = PublishSubject<Void>()
+    
+//    private let activityIndicator = ActivityIndicator()
+//    private let errorTracker = ErrorTracker()
     
     init(navigator: CreateJanjiNavigator) {
         self.navigator = navigator
         self.navigator.finish = backS
         
+        let errorTracker = ErrorTracker()
+        let activityIndicator = ActivityIndicator()
+        
         backI = backS.asObserver()
+        doneI = doneS.asObserver()
+        titleI = titleS.asObserver()
+        bodyI = bodyS.asObserver()
+        viewWillAppearI = viewWillAppearS.asObserver()
+        
+        // MARK
+        // Get user data from cloud and Local
+        let local: Observable<UserResponse> = AppState.local(key: .me)
+        let cloud = NetworkService.instance.requestObject(
+            PantauAuthAPI.me,
+            c: BaseResponse<UserResponse>.self)
+            .map({ $0.data })
+            .do(onSuccess: { (response) in
+                AppState.saveMe(response)
+            }, onError: { (e) in
+                print(e.localizedDescription)
+            })
+            .trackError(errorTracker)
+            .trackActivity(activityIndicator)
+            .asObservable()
+            .catchErrorJustComplete()
+        
+        let userData = viewWillAppearS
+            .flatMapLatest({ Observable.merge(local, cloud)})
+    
+        
+        let enablePost = Observable.combineLatest(titleS, bodyS)
+            .map { (title, body) -> Bool in
+                return title.count > 0 && body.count > 0
+            }
+            .startWith(false)
+        
+        let done = doneS
+            .withLatestFrom(Observable.combineLatest(titleS, bodyS))
+            .flatMapLatest({(title, desc) -> Driver<Void> in
+                return NetworkService.instance.requestObject(
+                    LinimasaAPI.createJanjiPolitiks(
+                        title: title,
+                        body: desc,
+                        image: nil),
+                    c: BaseResponse<JanjiPolitikResponse>.self)
+                    .trackError(errorTracker)
+                    .trackActivity(activityIndicator)
+                    .mapToVoid()
+                    .asDriverOnErrorJustComplete()
+            })
+            .do(onNext: { (_) in
+                navigator.back()
+            })
+            .asDriverOnErrorJustComplete()
+        
+        userDataO = userData.asDriverOnErrorJustComplete()
+        errorO = errorTracker.asDriver()
+        enableO = enablePost.asDriverOnErrorJustComplete()
+        doneO = done
     }
     
 }
