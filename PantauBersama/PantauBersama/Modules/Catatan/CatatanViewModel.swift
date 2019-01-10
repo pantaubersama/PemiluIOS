b9457038b7bf0a7c209f8e29d36f294c2539d456
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-01-10 20:26:59 +0700

    init note preference

--- PantauBersama/PantauBersama/Modules/Catatan/CatatanViewModel.swift
+++ PantauBersama/PantauBersama/Modules/Catatan/CatatanViewModel.swift
@@ -18,25 +18,78 @@ class CatatanViewModel: ViewModelType {
     
     struct Input {
         let backI: AnyObserver<Void>
+        let viewWillAppearI: AnyObserver<Void>
+        let notePreferenceI: AnyObserver<String>
+        let notePreferenceValueI: BehaviorSubject<Int>
     }
     
     struct Output {
-        
+        let totalResultO: Driver<TrendResponse>
+        let userDataO: Driver<UserResponse>
+        let notePreferenceO: Driver<String>
+        let notePreferenceValueO: Driver<Int>
     }
     
     private var navigator: CatatanNavigator
     private let backS = PublishSubject<Void>()
+    private let errorTracker = ErrorTracker()
+    private let activityIndicator = ActivityIndicator()
+    private let viewWillAppearS = PublishSubject<Void>()
+    private let notePreferenceS = PublishSubject<String>()
+    private let notePreferenceValueS = BehaviorSubject<Int>(value: 0)
     
     init(navigator: CatatanNavigator) {
         self.navigator = navigator
         self.navigator.finish = backS
         
-        
         input = Input(
-            backI: backS.asObserver()
+            backI: backS.asObserver(),
+            viewWillAppearI: viewWillAppearS.asObserver(),
+            notePreferenceI: notePreferenceS.asObserver(),
+            notePreferenceValueI: notePreferenceValueS.asObserver()
         )
         
-        output = Output()
+        // MARK
+        // Get Local user data
+        let local: Observable<UserResponse> = AppState.local(key: .me)
+        let cloud = NetworkService.instance
+            .requestObject(PantauAuthAPI.me,
+                           c: BaseResponse<UserResponse>.self)
+            .map({ $0.data })
+            .do(onSuccess: { (response) in
+                AppState.saveMe(response)
+            })
+            .trackError(errorTracker)
+            .trackActivity(activityIndicator)
+            .asObservable()
+            .catchErrorJustComplete()
+        
+        let userData = viewWillAppearS
+            .flatMapLatest({ Observable.merge(local, cloud) })
+        
+        let totalResult = viewWillAppearS
+            .flatMapLatest({ self.totalResult() })
+            .asDriverOnErrorJustComplete()
+        
+        let note = notePreferenceS
+            .asDriverOnErrorJustComplete()
+        
+        
+        output = Output(totalResultO: totalResult,
+                        userDataO: userData.asDriverOnErrorJustComplete(),
+                        notePreferenceO: note,
+                        notePreferenceValueO: notePreferenceValueS.asDriverOnErrorJustComplete())
+    }
+    
+    private func totalResult() -> Observable<TrendResponse> {
+        return NetworkService.instance
+            .requestObject(QuizAPI.getTotalResult(),
+                           c: BaseResponse<TrendResponse>.self)
+            .map({ $0.data })
+            .trackError(errorTracker)
+            .trackActivity(activityIndicator)
+            .asObservable()
+            .catchErrorJustComplete()
     }
     
 }
--- PantauBersama/PantauBersama/Modules/Catatan/ViewController/CatatanController.swift
+++ PantauBersama/PantauBersama/Modules/Catatan/ViewController/CatatanController.swift
@@ -9,10 +9,20 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Common
 
 class CatatanController: UIViewController {
     
     var viewModel: CatatanViewModel!
+    @IBOutlet weak var iconPreference: UIImageView!
+    @IBOutlet weak var labelPreference: Label!
+    @IBOutlet weak var labelPercentage: Label!
+    @IBOutlet weak var buttonPaslonSatu: Button!
+    @IBOutlet weak var buttonPaslonDua: Button!
+    @IBOutlet weak var buttonGolput: Button!
+    @IBOutlet weak var notePreference: Label!
+    @IBOutlet weak var containerPaslonSatu: UIView!
+    @IBOutlet weak var containerPaslonDua: UIView!
     
     private let disposeBag: DisposeBag = DisposeBag()
     
@@ -28,10 +38,125 @@ class CatatanController: UIViewController {
             .bind(to: viewModel.input.backI)
             .disposed(by: disposeBag)
         
+        buttonPaslonSatu.rx.tap
+            .map {(1)}
+            .do(onNext: { [weak self] (_) in
+                self?.viewModel.input.notePreferenceI.onNext(("(Jokowi - Makruf)"))
+            })
+            .bind(to: viewModel.input.notePreferenceValueI)
+            .disposed(by: disposeBag)
+        
+        buttonPaslonDua.rx.tap
+            .map {(2)}
+            .do(onNext: { [weak self] (_) in
+                self?.viewModel.input.notePreferenceI.onNext(("(Prabowo - Sandi)"))
+            })
+            .bind(to: viewModel.input.notePreferenceValueI)
+            .disposed(by: disposeBag)
+        
+        buttonGolput.rx.tap
+            .map {(3)}
+            .do(onNext: { [weak self] (_) in
+                self?.viewModel.input.notePreferenceI.onNext(("(belum menentukan pilihan)"))
+            })
+            .bind(to: viewModel.input.notePreferenceValueI)
+            .disposed(by: disposeBag)
+        
+        viewModel.output.totalResultO
+            .drive(onNext: { [weak self] (response) in
+                guard let `self` = self else { return }
+                let preference = response.teams.max { $0.percentage?.isLess(than: $1.percentage ?? 0.0) ?? false }
+                if let avatarUrl = preference?.team.avatar {
+                    self.iconPreference.af_setImage(withURL: URL(string: avatarUrl)!)
+                }
+                self.labelPreference.text = "\(response.meta.quizzes.finished) dari \(response.meta.quizzes.total) Kuis"
+                self.labelPercentage.text = "\(Double(preference?.percentage ?? 0.0)) (\(preference?.team.title ?? ""))"
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.output.userDataO
+            .drive(onNext: { [weak self] (response) in
+                guard let `self` = self else { return }
+                if let votePreference = response.user.votePreference {
+                    switch votePreference {
+                    case 0:
+                        self.buttonPaslonSatu.layer.borderColor = Color.orange_warm.cgColor
+                        self.buttonPaslonSatu.setTitleColor(Color.orange_warm, for: .normal)
+                        self.containerPaslonSatu.layer.borderColor = Color.orange_warm.cgColor
+                        self.notePreference.text = "(Jokowi - Makruf)"
+                    case 1:
+                        self.buttonPaslonDua.layer.borderColor = Color.orange_warm.cgColor
+                        self.buttonPaslonDua.setTitleColor(Color.orange_warm, for: .normal)
+                        self.containerPaslonDua.layer.borderColor = Color.orange_warm.cgColor
+                        self.notePreference.text = "(Prabowo - Sandi)"
+                    case 3:
+                        self.buttonGolput.layer.borderColor = Color.orange_warm.cgColor
+                        self.buttonGolput.setTitleColor(Color.orange_warm, for: .normal)
+                        self.notePreference.text = "(belum menentukan pilihan)"
+                    default: break
+                    }
+                }
+                
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.output.notePreferenceO
+            .drive(onNext: { [weak self] (s) in
+                guard let `self` = self else { return }
+                self.notePreference.text = s
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.output.notePreferenceValueO
+            .drive(onNext: { [weak self] (i) in
+                guard let `self` = self else { return }
+                switch i {
+                case 1:
+                    self.buttonPaslonSatu.layer.borderColor = Color.orange_warm.cgColor
+                    self.buttonPaslonSatu.setTitleColor(Color.orange_warm, for: .normal)
+                    self.containerPaslonSatu.layer.borderColor = Color.orange_warm.cgColor
+                    self.buttonPaslonDua.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonPaslonDua.setTitleColor(Color.grey_five, for: .normal)
+                    self.containerPaslonDua.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonGolput.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonGolput.setTitleColor(Color.grey_five, for: .normal)
+                case 2:
+                    self.buttonPaslonDua.layer.borderColor = Color.orange_warm.cgColor
+                    self.buttonPaslonDua.setTitleColor(Color.orange_warm, for: .normal)
+                    self.containerPaslonDua.layer.borderColor = Color.orange_warm.cgColor
+                    self.buttonPaslonSatu.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonPaslonSatu.setTitleColor(Color.grey_five, for: .normal)
+                    self.containerPaslonSatu.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonGolput.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonGolput.setTitleColor(Color.grey_five, for: .normal)
+                case 3:
+                    self.buttonGolput.layer.borderColor = Color.orange_warm.cgColor
+                    self.buttonGolput.setTitleColor(Color.orange_warm, for: .normal)
+                    self.buttonPaslonSatu.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonPaslonSatu.setTitleColor(Color.grey_five, for: .normal)
+                    self.containerPaslonSatu.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonPaslonDua.layer.borderColor = Color.grey_five.cgColor
+                    self.buttonPaslonDua.setTitleColor(Color.grey_five, for: .normal)
+                    self.containerPaslonDua.layer.borderColor = Color.grey_five.cgColor
+                default: break
+                }
+            })
+            .disposed(by: disposeBag)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        viewModel.input.viewWillAppearI.onNext(())
+    }
+    
+}
+
+
+extension CatatanController {
+    
+    private func resetButton() {
+        
     }
     
 }
