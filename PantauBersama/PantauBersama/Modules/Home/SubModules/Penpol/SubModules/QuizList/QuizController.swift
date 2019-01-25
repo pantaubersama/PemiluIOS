e350d673e914f6317b5a03c554e66d1362d9d630
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-01-25 16:05:29 +0700

    handle double check, if reproduce again need change strategy

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Penpol/SubModules/QuizList/QuizController.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Penpol/SubModules/QuizList/QuizController.swift
@@ -45,6 +45,8 @@ class QuizController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        viewModel.input.loadQuizTrigger.onNext(())
+        
         tableView.delegate = nil
         tableView.dataSource = nil
         viewModel.output.quizzes
