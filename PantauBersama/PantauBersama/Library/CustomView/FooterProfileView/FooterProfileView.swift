cad144d54bb63087232e1ebbbefdaeceba848765
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-02-14 17:15:14 +0700

    init component footer profile in challenge

---
+++ PantauBersama/PantauBersama/Library/CustomView/FooterProfileView/FooterProfileView.swift
@@ -0,0 +1,38 @@
+//
+//  FooterProfileView.swift
+//  PantauBersama
+//
+//  Created by Hanif Sugiyanto on 14/02/19.
+//  Copyright © 2019 PantauBersama. All rights reserved.
+//
+
+import UIKit
+import Common
+
+class FooterProfileView: UIView {
+    
+    @IBOutlet weak var ivAvatar: CircularUIImageView!
+    @IBOutlet weak var lblName: Label!
+    @IBOutlet weak var lblStatus: Label!
+    
+    
+    // Height 200.0
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        let view = loadNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+
+    
+}
