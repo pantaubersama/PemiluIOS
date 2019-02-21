36808ff56636e371ed52e7e6a6b4ea23acb1f4e3
Rahardyan Bisma <amsibsam@gmail.com>
2019-02-21 09:30:21 +0700

    add clap lottie

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Wordstadium/SubModules/LiveDebat/Components/ArgumentRightCell.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Wordstadium/SubModules/LiveDebat/Components/ArgumentRightCell.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 import Common
+import RxSwift
+import RxCocoa
+import Lottie
 
 class ArgumentRightCell: UITableViewCell, IReusableCell {
     @IBOutlet weak var lbArgument: Label!
     @IBOutlet weak var lbCreatedAt: Label!
     @IBOutlet weak var lbReadEstimation: Label!
+    @IBOutlet weak var viewClapLottie: UIView!
     @IBOutlet weak var btnClap: ImageButton!
     @IBOutlet weak var lbClapCount: Label!
     @IBOutlet weak var lbClapStatus: Label!
     
+    lazy private var clapAnimation: LOTAnimationView = {
+        let clapAnimation = LOTAnimationView(name: "clap")
+        clapAnimation.translatesAutoresizingMaskIntoConstraints = false
+        clapAnimation.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        clapAnimation.contentMode = .center
+        clapAnimation.frame = viewClapLottie.bounds
+        clapAnimation.loopAnimation = false
+        
+        return clapAnimation
+    }()
+    
+    private let disposeBag = DisposeBag()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        viewClapLottie.addSubview(clapAnimation)
+        configureConstraint()
+        
+        btnClap.rx.tap
+            .bind{ [unowned self] in
+                self.clapAnimation.play()
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureConstraint() {
+        NSLayoutConstraint.activate([
+            clapAnimation.trailingAnchor.constraint(equalTo: viewClapLottie.trailingAnchor),
+            clapAnimation.topAnchor.constraint(equalTo: viewClapLottie.topAnchor),
+            clapAnimation.leadingAnchor.constraint(equalTo: viewClapLottie.leadingAnchor),
+            clapAnimation.bottomAnchor.constraint(equalTo: viewClapLottie.bottomAnchor)
+            ])
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
