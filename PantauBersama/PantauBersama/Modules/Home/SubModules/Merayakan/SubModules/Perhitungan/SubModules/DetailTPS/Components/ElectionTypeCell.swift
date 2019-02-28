7f408aa5b53dc2b34acc24aaecae3b942382575a
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-02-28 20:05:49 +0700

    layout election type selection cell

---
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/Components/ElectionTypeCell.swift
@@ -0,0 +1,23 @@
+//
+//  ElectionTypeCell.swift
+//  PantauBersama
+//
+//  Created by Nanang Rafsanjani on 28/02/19.
+//  Copyright © 2019 PantauBersama. All rights reserved.
+//
+
+import UIKit
+import Common
+
+class ElectionTypeCell: UITableViewCell, IReusableCell {
+    @IBOutlet weak var lblTitle: Label!
+    @IBOutlet weak var btnFirstOption: Button!
+    @IBOutlet weak var btnSecOption: Button!
+    
+    func configureCell(item: ElectionType) {
+        lblTitle.text = item.title
+        btnFirstOption.setImage(item.imgFirstOption, for: .normal)
+        btnSecOption.setImage(item.imgSecOption, for: .normal)
+    }
+    
+}
