5b6b8d14e7cd8a0e6c63f614091e2a754a9042a0
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-02-28 20:05:10 +0700

    layout detail tps footer (upload button)

---
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/Components/DetailTPSFooter.swift
@@ -0,0 +1,15 @@
+//
+//  DetailTPSFooter.swift
+//  PantauBersama
+//
+//  Created by Nanang Rafsanjani on 28/02/19.
+//  Copyright © 2019 PantauBersama. All rights reserved.
+//
+
+import UIKit
+import Common
+
+class DetailTPSFooter: UITableViewCell, IReusableCell {
+    @IBOutlet weak var btnUpload: Button!
+    
+}
