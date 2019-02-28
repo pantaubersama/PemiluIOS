fbb34ddfe7fd0fe394204a00fae3b5422587a8e7
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-02-28 20:05:30 +0700

    create detail tps header (location info)

---
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/Components/DetailTPSHeader.swift
@@ -0,0 +1,22 @@
+//
+//  DetailTPSHeader.swift
+//  PantauBersama
+//
+//  Created by Nanang Rafsanjani on 28/02/19.
+//  Copyright © 2019 PantauBersama. All rights reserved.
+//
+
+import UIKit
+import Common
+
+class DetailTPSHeader: UITableViewCell, IReusableCell {
+    @IBOutlet weak var lblTitle: Label!
+    @IBOutlet weak var lblProvinsi: Label!
+    @IBOutlet weak var lblKota: Label!
+    @IBOutlet weak var lblKecamatan: Label!
+    @IBOutlet weak var lblDesa: Label!
+    @IBOutlet weak var btnOption: Button!
+
+    
+    
+}
