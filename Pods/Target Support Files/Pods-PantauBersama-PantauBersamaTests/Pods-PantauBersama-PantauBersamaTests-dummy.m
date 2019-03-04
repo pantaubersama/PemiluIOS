90fcf6bb80970f06f1e05064e4cbe21589a91cd0
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-03-04 16:43:57 +0700

    ignore Pods/

--- Pods/Target Support Files/Pods-PantauBersama-PantauBersamaTests/Pods-PantauBersama-PantauBersamaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PantauBersama_PantauBersamaTests : NSObject
-@end
-@implementation PodsDummy_Pods_PantauBersama_PantauBersamaTests
-@end
--- Pods/Target Support Files/Pods-PantauBersama/Pods-PantauBersama-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PantauBersama : NSObject
-@end
-@implementation PodsDummy_Pods_PantauBersama
-@end
