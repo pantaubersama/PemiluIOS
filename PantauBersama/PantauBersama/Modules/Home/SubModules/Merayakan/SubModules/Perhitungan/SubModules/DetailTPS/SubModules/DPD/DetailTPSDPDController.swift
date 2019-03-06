73301e891dab520caa1de546a43096e1a295283b
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-03-06 17:50:55 +0700

    implement DPD layout

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/DPD/DetailTPSDPDController.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/DPD/DetailTPSDPDController.swift
@@ -7,24 +7,90 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DetailTPSDPDController: UIViewController {
-
+    var viewModel: DetailTPSDPDViewModel!
+    
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    lazy var footer = UIView.nib(withType: DetailTPSDPRFooter.self)
+    lazy var header = UIView.nib(withType: DetailTPSDPRHeader.self)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = "DPD"
+        
+        let back = UIBarButtonItem(image: #imageLiteral(resourceName: "back"), style: .plain, target: nil, action: nil)
+        
+        navigationItem.leftBarButtonItem = back
+        navigationController?.navigationBar.configure(with: .white)
+        
+        back.rx.tap
+            .bind(to: viewModel.input.backI)
+            .disposed(by: disposeBag)
+        
+        tableView.setTableHeaderView(headerView: header)
+        tableView.setTableFooterView(footerView: footer)
+        tableView.registerReusableCell(TPSInputCell.self)
+        
+        viewModel.output.backO
+            .drive()
+            .disposed(by: disposeBag)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.updateHeaderViewFrame()
     }
-    */
+}
 
+extension DetailTPSDPDController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell() as TPSInputCell
+        return cell
+    }
+}
+
+extension UITableView {
+    
+    /// Set table header view & add Auto layout.
+    func setTableHeaderView(headerView: UIView) {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Set first.
+        self.tableHeaderView = headerView
+        
+        // Then setup AutoLayout.
+        headerView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        headerView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        headerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        
+        updateHeaderViewFrame()
+    }
+    
+    /// Update header view's frame.
+    func updateHeaderViewFrame() {
+        guard let headerView = self.tableHeaderView else { return }
+        
+        // Update the size of the header based on its internal content.
+        headerView.layoutIfNeeded()
+        
+        // ***Trigger table view to know that header should be updated.
+        let header = self.tableHeaderView
+        self.tableHeaderView = header
+    }
+    
+    func setTableFooterView(footerView: UIView) {
+        self.tableFooterView = footerView
+    }
 }
--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/DPD/DetailTPSDPDCoordinator.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/DPD/DetailTPSDPDCoordinator.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 PantauBersama. All rights reserved.
 //
 
-import Foundation
+import Common
+import RxSwift
+import RxCocoa
+
+protocol DetailTPSDPDNavigator {
+    func back() -> Observable<Void>
+}
+
+class DetailTPSDPDCoordinator: BaseCoordinator<Void> {
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    override func start() -> Observable<Void> {
+        let viewController = DetailTPSDPDController()
+        let viewModel = DetailTPSDPDViewModel(navigator: self)
+        viewController.viewModel = viewModel
+        viewController.hidesBottomBarWhenPushed = true
+        
+        navigationController.pushViewController(viewController, animated: true)
+        return Observable.never()
+    }
+}
+
+extension DetailTPSDPDCoordinator: DetailTPSDPDNavigator {
+    func back() -> Observable<Void> {
+        self.navigationController.popViewController(animated: true)
+        return Observable.empty()
+    }
+}
