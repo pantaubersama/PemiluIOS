2030c3ab0fc95604bae05f08fad1ada0f8c91507
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-03-06 17:51:56 +0700

    implement c1 form layout

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/InputC1/C1InputFormController.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/InputC1/C1InputFormController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 import Common
+import RxSwift
+import RxCocoa
+
+enum FormC1Type {
+    case presiden
+    case dpr
+    case dpd
+    case dprdKota
+    case dprdProv
+    
+    var title: String {
+        switch self {
+        case .dpd:
+            return "DPD"
+        case .dpr:
+            return "DPR RI"
+        case .dprdKota:
+            return "DPRD KAB/KOTA"
+        case .dprdProv:
+            return "DPRD PROVINSI"
+        case .presiden:
+            return "PRESIDEN"
+        }
+    }
+}
 
 class C1InputFormController: UIViewController {
+    var viewModel: C1InputFormViewModel!
+    var type: FormC1Type!
+    
+    private let disposeBag = DisposeBag()
+    
     @IBOutlet weak var containerView: UIStackView!
     
     lazy var pemilihView = UIView.nib(withType: C1PemilihView.self)
@@ -19,10 +49,23 @@ class C1InputFormController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Model C1 Presiden"
+        title = type.title
+        
+        let back = UIBarButtonItem(image: #imageLiteral(resourceName: "back"), style: .plain, target: nil, action: nil)
+        
+        navigationItem.leftBarButtonItem = back
+        navigationController?.navigationBar.configure(with: .white)
+        
+        back.rx.tap
+            .bind(to: viewModel.input.backI)
+            .disposed(by: disposeBag)
 
         containerView.addArrangedSubview(pemilihView)
         containerView.addArrangedSubview(pemilihDisabilitasView)
         containerView.addArrangedSubview(suratSuaraView)
+        
+        viewModel.output.backO
+            .drive()
+            .disposed(by: disposeBag)
     }
 }
--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/InputC1/C1InputFormViewModel.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/DetailTPS/SubModules/InputC1/C1InputFormViewModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import Common
+import RxSwift
+import RxCocoa
+
+class C1InputFormViewModel: ViewModelType {
+    struct Input {
+        let backI: AnyObserver<Void>
+    }
+    
+    struct Output {
+        let backO: Driver<Void>
+    }
+    
+    var input: Input
+    var output: Output
+    
+    private let navigator: C1InputFormNavigator
+    private let backS = PublishSubject<Void>()
+    private let detailTPSS = PublishSubject<Void>()
+    
+    init(navigator: C1InputFormNavigator) {
+        self.navigator = navigator
+        
+        input = Input(backI: backS.asObserver())
+        
+        let back = backS
+            .flatMap({ navigator.back() })
+            .asDriverOnErrorJustComplete()
+        
+        output = Output(backO: back)
+    }
+}
