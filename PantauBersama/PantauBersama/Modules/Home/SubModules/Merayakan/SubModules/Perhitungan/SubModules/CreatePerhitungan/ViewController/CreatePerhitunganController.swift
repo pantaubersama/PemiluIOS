a59b9db563e1db085c919555e31a0b879a0fa1f9
Nanang Rafsanjani <jukiginanjar@hotmail.com>
2019-03-19 14:36:21 +0700

    update create perhitungan view controller

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/CreatePerhitungan/ViewController/CreatePerhitunganController.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Perhitungan/SubModules/CreatePerhitungan/ViewController/CreatePerhitunganController.swift
@@ -15,9 +15,17 @@ class CreatePerhitunganController: UIViewController {
     var viewModel: CreatePerhitunganViewModel!
     private let disposeBag = DisposeBag()
     
+    @IBOutlet private var noTpsTF: TextField!
+    @IBOutlet private var provinsiTF: TextField!
+    @IBOutlet private var kabupatenTF: TextField!
+    @IBOutlet private var kecamatanTF: TextField!
+    @IBOutlet private var desaTF: TextField!
+    
+    @IBOutlet private var addressLabel: Label!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Data TPS"
         let back = UIBarButtonItem(image: #imageLiteral(resourceName: "back"), style: .plain, target: nil, action: nil)
         let done = UIBarButtonItem(image: #imageLiteral(resourceName: "icDoneRed"), style: .plain, target: nil, action: nil)
@@ -41,6 +49,72 @@ class CreatePerhitunganController: UIViewController {
         viewModel.output.detailTPSO
             .drive()
             .disposed(by: disposeBag)
+        
+        viewModel.formattedAddress
+            .asObservable()
+            .bind(to: addressLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.getProvinces()
+        
+        viewModel.updateLocation()
     }
+}
 
+extension CreatePerhitunganController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField == provinsiTF {
+            let dialog = SelectionDialog(title: "Provinsi", closeButtonTitle: "Tutup")
+            viewModel.provinces.forEach({ [weak self] (province) in
+                let handler = {
+                    self?.viewModel.selectedProvince = province
+                    self?.provinsiTF.text = province.name
+                    dialog.close()
+                }
+                dialog.addItem(item: province.name, didTapHandler: handler)
+            })
+            dialog.show()
+            return false
+            
+        } else if textField == kabupatenTF {
+            let dialog = SelectionDialog(title: "Kabupaten/Kota", closeButtonTitle: "Tutup")
+            viewModel.regencies.forEach({ [weak self] (reg) in
+                let handler = {
+                    self?.viewModel.selectedRegency = reg
+                    self?.kabupatenTF.text = reg.name
+                    dialog.close()
+                }
+                dialog.addItem(item: reg.name, didTapHandler: handler)
+            })
+            dialog.show()
+            return false
+            
+        } else if textField == kecamatanTF {
+            let dialog = SelectionDialog(title: "Kecamatan", closeButtonTitle: "Tutup")
+            viewModel.districts.forEach({ [weak self] (district) in
+                let handler = {
+                    self?.viewModel.selectedDistrict = district
+                    self?.kecamatanTF.text = district.name
+                    dialog.close()
+                }
+                dialog.addItem(item: district.name, didTapHandler: handler)
+            })
+            dialog.show()
+            return false
+            
+        } else if textField == desaTF {
+            let dialog = SelectionDialog(title: "Kelurahan/Desa", closeButtonTitle: "Tutup")
+            viewModel.villages.forEach({ [weak self] (village) in
+                let handler = {
+                    self?.viewModel.selectedVillage = village
+                    self?.desaTF.text = village.name
+                    dialog.close()
+                }
+                dialog.addItem(item: village.name, didTapHandler: handler)
+            })
+            dialog.show()
+            return false
+        }
+        return true
+    }
 }
