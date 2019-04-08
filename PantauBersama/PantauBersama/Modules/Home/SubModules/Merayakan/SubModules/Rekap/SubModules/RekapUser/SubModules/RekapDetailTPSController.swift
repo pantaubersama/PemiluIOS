9ec4f5e78192b8d94f184f054daac446295dd5af
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-04-08 17:34:33 +0700

    comfig some miss match layout in C1

---
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Rekap/SubModules/RekapUser/SubModules/RekapDetailTPSController.swift
@@ -0,0 +1,9 @@
+//
+//  RekapDetailTPSController.swift
+//  PantauBersama
+//
+//  Created by Hanif Sugiyanto on 08/04/19.
+//  Copyright © 2019 PantauBersama. All rights reserved.
+//
+
+import Foundation
