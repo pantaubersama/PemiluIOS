ad4c299dfe75f961d19816687dc12e637b1cc9ae
Muhammad Hanif Sugiyanto <hanif@extrainteger.com>
2019-04-15 16:39:20 +0700

    adding empty vieews viillages

--- PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Rekap/SubModules/RekapList/Village/RekapListVillageController.swift
+++ PantauBersama/PantauBersama/Modules/Home/SubModules/Merayakan/SubModules/Rekap/SubModules/RekapList/Village/RekapListVillageController.swift
@@ -15,6 +15,8 @@ class RekapListVillageController: UITableViewController {
     var viewModel: RekapListVillageViewModel!
     private let disposeBag = DisposeBag()
     
+    private lazy var emptyView = EmptyView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = nil
@@ -34,8 +36,14 @@ class RekapListVillageController: UITableViewController {
             .disposed(by: disposeBag)
         
         viewModel.output.itemsO
-            .do(onNext: { [weak self] (_) in
+            .do(onNext: { [weak self] (items) in
                 guard let `self` = self else { return }
+                if items.count == 0 {
+                    self.emptyView.frame = self.tableView.bounds
+                    self.tableView.backgroundView = self.emptyView
+                } else {
+                    self.tableView.backgroundView = nil
+                }
                 self.tableView.refreshControl?.endRefreshing()
             })
             .drive(tableView.rx.items) { tableView, row, item in
